d54c47984ac20f52757da052a8e6f3ac58b6fcce
ntorki-dev <ntorki@gmail.com>
2025-09-22 23:42:27 +0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
   // === Non-conflicting UI version ===
   try{
     if (!window.__MS_UI_VERSION) {
-      window.__MS_UI_VERSION = 'v52.2';
+      window.__MS_UI_VERSION = 'v52.3';
       var _h = document.getElementById('hostLog');
       if (_h) _h.textContent = (_h.textContent? _h.textContent+'\n':'') + 'UI version: ' + window.__MS_UI_VERSION;
     }
@@ -45,7 +45,7 @@
       if (_j) _j.textContent = (_j.textContent? _j.textContent+'
 ':'') + 'UI version: ' + window.__MS_UI_VERSION;
 // === UI build version ===
-  const MS_UI_VERSION = 'v52.2';
+  const MS_UI_VERSION = 'v52.3';
   try {
     const h = document.getElementById('hostLog'); if (h) h.textContent = (h.textContent? h.textContent+'\n':'') + 'UI version: ' + MS_UI_VERSION;
     const j = document.getElementById('joinLog'); if (j) j.textContent = (j.textContent? j.textContent+'\n':'') + 'UI version: ' + MS_UI_VERSION;
