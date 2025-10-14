115dd475c155cc555738c355f1309213796cc1ba
Nader <ntorki@gmail.com>
2025-10-14 23:56:18 +0400

    Update home.js

--- newui/home.js
+++ newui/home.js
@@ -22,7 +22,7 @@ export async function render () {
 
   const learn = (
     '<div class="home-learn">' +
-      '<br> </br><br> </br><a href="https://www.matchsqr.com/about" target="_blank" class="learn-link">learn more</a> about MatchSqr' +
+      '<br> </br><a href="https://www.matchsqr.com/about" target="_blank" class="learn-link">learn more</a> about MatchSqr' +
     '<br> </br><br> </br></div>'
   );
 
